f7c1beeb3494d380304b11f0830fb382c6162bae
JF-Rabbit <qwertjkl9090@163.com>
2017-07-17 13:46:44 +0800

    add RobotUtil.java

---
+++ sagittarius-common/src/main/java/org/sagittarius/common/robot/RobotUtil.java
@@ -0,0 +1,32 @@
+package org.sagittarius.common.robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.sagittarius.common.Delay;
+
+public class RobotUtil {
+
+	/**
+	 * move to startX|startY, click and hold to endX|endY, release mouse
+	 * 
+	 * @param startX
+	 * @param startY
+	 * @param endX
+	 * @param endY
+	 * @throws AWTException
+	 */
+	public static void dragToLocation(int startX, int startY, int endX, int endY) throws AWTException {
+		Robot robot = new Robot();
+		robot.mouseMove(startX, startY);
+		Delay.sleep(100);
+		robot.mousePress(KeyEvent.BUTTON1_MASK);
+		Delay.sleep(100);
+		robot.mouseMove(endX, endY);
+		Delay.sleep(100);
+		robot.mouseWheel(1);
+		Delay.sleep(100);
+		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+	}
+}
