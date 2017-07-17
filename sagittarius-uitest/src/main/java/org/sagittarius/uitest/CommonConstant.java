7fac45544a801f018e0d4fece12636e4714d8fa8
JF-Rabbit <qwertjkl9090@163.com>
2017-07-17 16:48:48 +0800

    add WebTest(Parent Class), add CommonConstant

---
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/CommonConstant.java
@@ -0,0 +1,6 @@
+package org.sagittarius.uitest;
+
+public interface CommonConstant {
+
+	String SPRING_PATH = "classpath:spring.xml";
+}
---
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/WebTest.java
@@ -0,0 +1,34 @@
+package org.sagittarius.uitest;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.sagittarius.uitest.driver.DriverManager;
+import org.sagittarius.uitest.exception.DriverInitException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = CommonConstant.SPRING_PATH)
+public class WebTest extends AbstractJUnit4SpringContextTests {
+
+	@Resource
+	DriverManager manager;
+	public WebDriver driver;
+
+	@Before
+	public void setup() throws DriverInitException, IOException {
+		driver = manager.getDriver();
+	}
+
+	@After
+	public void teardown() {
+		manager.quitDriver(driver);
+	}
+}
