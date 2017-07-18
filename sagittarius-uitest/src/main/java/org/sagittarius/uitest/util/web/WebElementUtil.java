66f984078ef8b8fc971ef27f5f01e02dac7e896f
JF-Rabbit <qwertjkl9090@163.com>
2017-07-18 15:39:42 +0800

    add method: clickDisableElement

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
@@ -1,6 +1,9 @@
 package org.sagittarius.uitest.util.web;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.sagittarius.common.robot.RobotUtil;
+import org.sagittarius.uitest.util.web.js.JsUtil;
 
 public class WebElementUtil {
 
@@ -10,6 +13,11 @@ public class WebElementUtil {
 		int height = element.getSize().height;
 		int width = element.getSize().width;
 		return new int[] { (x + width) / 2, (y + height) / 2 };
+	}
+
+	public static void clickDisableElement(WebDriver driver, WebElement element) {
+		int[] coordinate = getElementCenter(element);
+		RobotUtil.moveToClick(coordinate[0], coordinate[1] + JsUtil.getActualY(driver));
 	}
 
 }
