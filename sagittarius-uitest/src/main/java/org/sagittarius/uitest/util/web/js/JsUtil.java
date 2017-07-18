2f08b5b16a857cef6260d0cf1d1649ef7dd11fc4
JF-Rabbit <qwertjkl9090@163.com>
2017-07-18 15:39:20 +0800

    change name: getActualX => getActualY

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/js/JsUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/js/JsUtil.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.WebDriver;
 public class JsUtil implements JsConstant {
 
 	/**
-	 * 返回页面X原点在显示器的X坐标 
+	 * 返回页面Y原点在显示器的Y坐标 
 	 * 
 	 * @param driver
 	 * @return
 	 */
-	public static int getActualX(WebDriver driver) {
+	public static int getActualY(WebDriver driver) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		int workAreaHeight = Integer.valueOf(js.executeScript(WINDOW_SCREEN_AVAILHEIGHT).toString());
 		int webDisableHeight = Integer.valueOf(js.executeScript(DOCUMENT_BODY_CLIENTHEIGHT).toString());
