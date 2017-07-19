22620bd5108b25fd886d9deda03b7cee91117240
JF-Rabbit <qwertjkl9090@163.com>
2017-07-19 14:41:18 +0800

    add ENV flag

--- sagittarius-uitest-web/src/main/java/org/sagittarius/uitest/web/ConfigConstant.java
+++ sagittarius-uitest-web/src/main/java/org/sagittarius/uitest/web/ConfigConstant.java
@@ -3,6 +3,10 @@ package org.sagittarius.uitest.web;
 public interface ConfigConstant {
 
 	String CONFIG_FILE_PATH = "conf/config.properties";
+	
+	String ENV = "env";
+	String ENV_QA = "env.qa.";
+	String ENV_RD = "env.rd.";
 
 	String URL = "url";
 	String USERNAME = "username";
