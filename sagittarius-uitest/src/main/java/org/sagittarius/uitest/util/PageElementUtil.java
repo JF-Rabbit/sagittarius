cf0de20a896338d4c6b95a48972b8d5a55e82211
JF-Rabbit <qwertjkl9090@163.com>
2017-07-19 14:40:08 +0800

    add method: showPageSource

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/PageElementUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/PageElementUtil.java
@@ -8,11 +8,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.iOSXCUITBy;
 
 public class PageElementUtil {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PageElementUtil.class);
 
 	public static void initPages(WebDriver driver, PageUI... pageUIs) {
 		for (PageUI pageUI : pageUIs) {
@@ -44,5 +48,9 @@ public class PageElementUtil {
 		element.clear();
 		element.sendKeys(key);
 	}
-
+	
+	public static void showPageSource(WebDriver driver) {
+		logger.info(driver.getPageSource());
+	}
+	
 }
