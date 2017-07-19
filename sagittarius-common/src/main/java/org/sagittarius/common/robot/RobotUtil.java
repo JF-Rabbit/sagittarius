f85a450a6b2b5636a326aa3f7cb720afa1ebe20f
JF-Rabbit <qwertjkl9090@163.com>
2017-07-19 16:51:09 +0800

    add method: setClipboardData action_CONTROL_S_CONTROL_V_ENTER

--- sagittarius-common/src/main/java/org/sagittarius/common/robot/RobotUtil.java
+++ sagittarius-common/src/main/java/org/sagittarius/common/robot/RobotUtil.java
@@ -2,11 +2,17 @@ package org.sagittarius.common.robot;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 import org.sagittarius.common.Delay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RobotUtil {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RobotUtil.class);
 
 	/**
 	 * move to startX|startY, click and hold to endX|endY, release mouse
@@ -53,6 +59,28 @@ public class RobotUtil {
 			e.printStackTrace();
 		}
 		return robot;
+	}
+
+	public static void setClipboardData(String string) {
+		StringSelection ss = new StringSelection(string);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		logger.info("set msg to SystemClipboard:{}", string);
+	}
+
+	public static void action_CONTROL_S_CONTROL_V_ENTER() {
+		Robot robot = createRobot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_S);
+		robot.keyRelease(KeyEvent.VK_S);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		Delay.sleep(200);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		Delay.sleep(100);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
 	}
 
 }
