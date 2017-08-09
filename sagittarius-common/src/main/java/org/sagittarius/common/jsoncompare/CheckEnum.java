a946a06b64d2035bd1cc34833601b5676712f184
J_Rabbit <zhangfan@k2data.com.cn>
2017-08-09 15:41:11 +0800

    add CheckEnum, add FilterEnum

---
+++ sagittarius-common/src/main/java/org/sagittarius/common/jsoncompare/CheckEnum.java
@@ -0,0 +1,17 @@
+package org.sagittarius.common.jsoncompare;
+
+public enum CheckEnum {
+	IS_ANY_INTEGER,
+
+	IS_ANY_DOUBLE,
+
+	IS_ANY_STRING,
+
+	IS_TIMESTEMP,
+
+	MATCH_JSONDIFF,
+
+	MATCH_REGEX,
+
+	MATCH_RULE_REGEX
+}
---
+++ sagittarius-common/src/main/java/org/sagittarius/common/jsoncompare/FilterEnum.java
@@ -0,0 +1,19 @@
+package org.sagittarius.common.jsoncompare;
+
+public enum FilterEnum {
+	IGNORE_RESPONSE_CODE,
+
+	IGNORE_VALUE,
+
+	IGNORE_OBJECT_SIZE,
+
+	IGNORE_ARRAY_SIZE,
+
+//	CHECK_ARRAY_NO1,
+	
+	IS_JSON_OBJECT,
+
+	IS_JSON_ARRAY,
+	
+	IS_JSON_PRIMITIVE,
+}
