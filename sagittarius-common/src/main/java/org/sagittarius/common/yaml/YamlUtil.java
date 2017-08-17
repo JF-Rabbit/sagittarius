7e01a8b3d85b18b11417e4b5316936fcf0999eb6
J_Rabbit <zhangfan@k2data.com.cn>
2017-08-17 14:37:16 +0800

    add YamlUtil

---
+++ sagittarius-common/src/main/java/org/sagittarius/common/yaml/YamlUtil.java
@@ -0,0 +1,27 @@
+package org.sagittarius.common.yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class YamlUtil {
+
+	public static Iterable<Object> loadAll(String path) {
+		try {
+			return new Yaml().loadAll(new FileInputStream(new File(path)));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static <T> T loadAs(String path, Class<T> clazz) {
+		try {
+			T result = new Yaml().loadAs(new FileInputStream(new File(path)), clazz);
+			return result;
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
