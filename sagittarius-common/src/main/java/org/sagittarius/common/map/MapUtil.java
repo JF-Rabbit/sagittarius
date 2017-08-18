5db39aee79f70c9f63e537223da48ded7de5207d
J_Rabbit <zhangfan@k2data.com.cn>
2017-08-18 11:26:56 +0800

    add method map2Obj

--- sagittarius-common/src/main/java/org/sagittarius/common/map/MapUtil.java
+++ sagittarius-common/src/main/java/org/sagittarius/common/map/MapUtil.java
@@ -1,7 +1,9 @@
 package org.sagittarius.common.map;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 
+import org.sagittarius.common.reflect.ReflectUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,5 +22,24 @@ public class MapUtil {
 			logger.info("key:{}, value:{}", k, v);
 		});
 	}
-	
+
+	/**
+	 * Map映射成对象， 不匹配的字段默认为null
+	 * 
+	 * @param clazz
+	 *            实例类(必须有无参构造器)
+	 * @param map
+	 * @return T 实例对象
+	 */
+	public static <T> T map2Obj(Class<T> clazz, Map<String, Object> map) {
+		T t = ReflectUnit.newInstance(clazz);
+		for (Field field : clazz.getDeclaredFields()) {
+			if (map.containsKey(field.getName())) {
+				field.setAccessible(true);
+				ReflectUnit.setField(field, t, map.get(field.getName()));
+			}
+		}
+		return t;
+	}
+
 }
