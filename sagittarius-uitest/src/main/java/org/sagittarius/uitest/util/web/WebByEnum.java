1ac7fbe62dbd9746535cabd4406f65ff56ddd2cb
JFRabbit <qwertjkl9090@163.com>
2017-09-01 15:48:30 +0800

    update findElement method

---
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebByEnum.java
@@ -0,0 +1,5 @@
+package org.sagittarius.uitest.util.web;
+
+public enum WebByEnum {
+	XPATH, ID, CLASS_NAME, NAME, TAG_NAME, LINK_TEXT, CSS, PARTIA_LINK_TEXT
+}
--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
@@ -65,74 +65,124 @@ public class WebElementUtil {
 
 	}
 
-	private static void checkTimeout(int waitSecound) {
-		if (waitSecound <= DriverManager.DEFAULT_FIND_ELEMENT_TIMEOUT) {
+	private static void checkTimeout(int secound) {
+		if (secound <= DriverManager.DEFAULT_FIND_ELEMENT_TIMEOUT) {
 			throw new IllegalArgumentException(MULTIPLE_CLICKABLE_ELEMENT);
 		}
 	}
 
-	public static WebElement findElementByWait(WebDriver driver, int waitSecound, FindWebElementEnum findType, String findMsg) {
+	public static WebElement findElement(WebDriver driver, WebByEnum by, String msg) {
 
-		checkTimeout(waitSecound);
+		switch (by) {
+		case XPATH:
+			return driver.findElement(By.xpath(msg));
+		case ID:
+			return driver.findElement(By.id(msg));
+		case CLASS_NAME:
+			return driver.findElement(By.className(msg));
+		case NAME:
+			return driver.findElement(By.name(msg));
+		case TAG_NAME:
+			return driver.findElement(By.tagName(msg));
+		case LINK_TEXT:
+			return driver.findElement(By.linkText(msg));
+		case CSS:
+			return driver.findElement(By.cssSelector(msg));
+		case PARTIA_LINK_TEXT:
+			return driver.findElement(By.partialLinkText(msg));
+		default:
+			return null;
+		}
+	}
 
-		WebDriverWait wait = new WebDriverWait(driver, waitSecound);
+	public static List<WebElement> findElements(WebDriver driver, WebByEnum by, String msg) {
+
+		switch (by) {
+		case XPATH:
+			return driver.findElements(By.xpath(msg));
+		case ID:
+			return driver.findElements(By.id(msg));
+		case CLASS_NAME:
+			return driver.findElements(By.className(msg));
+		case NAME:
+			return driver.findElements(By.name(msg));
+		case TAG_NAME:
+			return driver.findElements(By.tagName(msg));
+		case LINK_TEXT:
+			return driver.findElements(By.linkText(msg));
+		case CSS:
+			return driver.findElements(By.cssSelector(msg));
+		case PARTIA_LINK_TEXT:
+			return driver.findElements(By.partialLinkText(msg));
+		default:
+			return null;
+		}
+	}
+
+	public static WebElement findElementByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
+
+		checkTimeout(secound);
+
+		WebDriverWait wait = new WebDriverWait(driver, secound);
 		return wait.until(new ExpectedCondition<WebElement>() {
 			public WebElement apply(WebDriver driver) {
-				switch (findType) {
-				case X_PATH:
-					return driver.findElement(By.xpath(findMsg));
+				switch (by) {
+				case XPATH:
+					return driver.findElement(By.xpath(msg));
 				case ID:
-					return driver.findElement(By.id(findMsg));
+					return driver.findElement(By.id(msg));
 				case CLASS_NAME:
-					return driver.findElement(By.className(findMsg));
+					return driver.findElement(By.className(msg));
 				case NAME:
-					return driver.findElement(By.name(findMsg));
+					return driver.findElement(By.name(msg));
 				case TAG_NAME:
-					return driver.findElement(By.tagName(findMsg));
+					return driver.findElement(By.tagName(msg));
 				case LINK_TEXT:
-					return driver.findElement(By.linkText(findMsg));
+					return driver.findElement(By.linkText(msg));
 				case CSS:
-					return driver.findElement(By.cssSelector(findMsg));
+					return driver.findElement(By.cssSelector(msg));
 				case PARTIA_LINK_TEXT:
-					return driver.findElement(By.partialLinkText(findMsg));
+					return driver.findElement(By.partialLinkText(msg));
+				default:
+					return null;
 				}
-				return null;
 			}
 		});
 	}
 
-	public static List<WebElement> findElementsByWait(WebDriver driver, int waitSecound, FindWebElementEnum findType, String findMsg) {
+	public static List<WebElement> findElementsByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
 
-		checkTimeout(waitSecound);
+		checkTimeout(secound);
 
-		WebDriverWait wait = new WebDriverWait(driver, waitSecound);
+		WebDriverWait wait = new WebDriverWait(driver, secound);
 		return wait.until(new ExpectedCondition<List<WebElement>>() {
 			public List<WebElement> apply(WebDriver driver) {
-				switch (findType) {
-				case X_PATH:
-					return driver.findElements(By.xpath(findMsg));
+				switch (by) {
+				case XPATH:
+					return driver.findElements(By.xpath(msg));
 				case ID:
-					return driver.findElements(By.id(findMsg));
+					return driver.findElements(By.id(msg));
 				case CLASS_NAME:
-					return driver.findElements(By.className(findMsg));
+					return driver.findElements(By.className(msg));
 				case NAME:
-					return driver.findElements(By.name(findMsg));
+					return driver.findElements(By.name(msg));
 				case TAG_NAME:
-					return driver.findElements(By.tagName(findMsg));
+					return driver.findElements(By.tagName(msg));
 				case LINK_TEXT:
-					return driver.findElements(By.linkText(findMsg));
+					return driver.findElements(By.linkText(msg));
 				case CSS:
-					return driver.findElements(By.cssSelector(findMsg));
+					return driver.findElements(By.cssSelector(msg));
 				case PARTIA_LINK_TEXT:
-					return driver.findElements(By.partialLinkText(findMsg));
+					return driver.findElements(By.partialLinkText(msg));
+				default:
+					return null;
 				}
-				return null;
 			}
 		});
 	}
 
-	public static void saveCurrentWebPage(String savePath) {
-		RobotUtil.setClipboardData(savePath);
+	public static void saveCurrentWebPage(String path) {
+		RobotUtil.setClipboardData(path);
 		RobotUtil.action_CONTROL_S_CONTROL_V_ENTER();
 	}
 
