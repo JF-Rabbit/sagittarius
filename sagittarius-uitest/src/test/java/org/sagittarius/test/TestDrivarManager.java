0f4bd4345c013f2f695272baa32601a5bd9d378f
JFRabbit <qwertjkl9090@163.com>
2017-10-18 11:14:48 +0800

    add test case for DriverManager

---
+++ sagittarius-uitest/src/test/java/org/sagittarius/test/TestDrivarManager.java
@@ -0,0 +1,13 @@
+package org.sagittarius.test;
+
+import org.sagittarius.uitest.WebTest;
+import org.testng.annotations.Test;
+
+public class TestDrivarManager extends WebTest{
+	
+	@Test
+	public void doTest1() {
+		
+	}
+
+}
