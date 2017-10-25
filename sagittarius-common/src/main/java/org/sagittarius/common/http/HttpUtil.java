6826ff06a096650d116b915307e5c048943b4d56
JFRabbit <qwertjkl9090@163.com>
2017-10-25 16:34:46 +0800

    modify code

--- sagittarius-common/src/main/java/org/sagittarius/common/http/HttpUtil.java
+++ sagittarius-common/src/main/java/org/sagittarius/common/http/HttpUtil.java
@@ -27,140 +27,137 @@ import org.apache.http.message.BasicHeader;
 // http://hc.apache.org/httpcomponents-client-ga/examples.html
 public class HttpUtil {
 
-	public static HttpResponseConfig service(HttpRequestConfig httpRequestConfig) throws HttpException {
-		CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
-		HttpResponseConfig httpResponseConfig = new HttpResponseConfig();
-		try {
+    public static HttpResponseConfig service(HttpRequestConfig httpRequestConfig) throws HttpException {
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        HttpResponseConfig httpResponseConfig = new HttpResponseConfig();
+        try {
 
-			URIBuilder builder = new URIBuilder(httpRequestConfig.getRequestUrl());
+            URIBuilder builder = new URIBuilder(httpRequestConfig.getRequestUrl());
 
-			HttpRequestBase httpRequest = null;
+            HttpRequestBase httpRequest = null;
 
-			switch (httpRequestConfig.getRequestMethod()) {
-			case GET:
-				builder = setParam(httpRequestConfig.getRequestParam(), builder);
-				HttpGet httpGet = new HttpGet(builder.build());
-				httpRequest = httpGet;
-				break;
-			case POST:
-				HttpPost httpPost = new HttpPost(builder.build());
-				httpPost.setEntity(new StringEntity(httpRequestConfig.getHttpEntityJsonStr(), "UTF-8"));
-				httpRequest = httpPost;
-				break;
-			case DELETE:
-				httpRequest = new HttpDelete(builder.build());
-				// TODO
-				break;
-			case PUT:
-				httpRequest = new HttpPut(builder.build());
-				// TODO
-				break;
-			}
+            switch (httpRequestConfig.getRequestMethod()) {
+                case GET:
+                    builder = setParam(httpRequestConfig.getRequestParam(), builder);
+                    httpRequest = new HttpGet(builder.build());
+                    break;
+                case POST:
+                    HttpPost httpPost = new HttpPost(builder.build());
+                    httpPost.setEntity(new StringEntity(httpRequestConfig.getHttpEntityJsonStr(), "UTF-8"));
+                    httpRequest = httpPost;
+                    break;
+                case DELETE:
+                    httpRequest = new HttpDelete(builder.build());
+                    // TODO
+                    break;
+                case PUT:
+                    httpRequest = new HttpPut(builder.build());
+                    // TODO
+                    break;
+            }
 
-			httpRequest.setHeaders(setHeaders(httpRequestConfig.getRequestHeaders()));
+            httpRequest.setHeaders(setHeaders(httpRequestConfig.getRequestHeaders()));
 
-			long start = System.currentTimeMillis();
-			HttpResponse httpResponse = closeableHttpClient.execute(httpRequest, httpRequestConfig.getContext());
-			long end = System.currentTimeMillis();
-			httpResponseConfig.setResponseTime(end - start);
+            long start = System.currentTimeMillis();
+            HttpResponse httpResponse = closeableHttpClient.execute(httpRequest, httpRequestConfig.getContext());
+            long end = System.currentTimeMillis();
+            httpResponseConfig.setResponseTime(end - start);
 
-			httpResponseConfig.setResponseStatusCode(httpResponse.getStatusLine().getStatusCode());
+            httpResponseConfig.setResponseStatusCode(httpResponse.getStatusLine().getStatusCode());
 
-			httpResponseConfig.setResponseHeaders(setHeadersMap(httpResponse.getAllHeaders()));
+            httpResponseConfig.setResponseHeaders(setHeadersMap(httpResponse.getAllHeaders()));
 
-			if (httpResponse.getEntity() != null) {
-				httpResponseConfig.setResponseContent(getContent(httpResponse.getEntity().getContent()));
-			}
+            if (httpResponse.getEntity() != null) {
+                httpResponseConfig.setResponseContent(getContent(httpResponse.getEntity().getContent()));
+            }
 
-		} catch (Exception e) {
-			throw new HttpException(e);
-		} finally {
-			try {
-				closeableHttpClient.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return httpResponseConfig;
-	}
+        } catch (Exception e) {
+            throw new HttpException(e);
+        } finally {
+            try {
+                closeableHttpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return httpResponseConfig;
+    }
 
-	// http://blog.csdn.net/qq32933432/article/details/51813256
-	// http://www.cnblogs.com/yaowen/p/3757571.html
+    // http://blog.csdn.net/qq32933432/article/details/51813256
+    // http://www.cnblogs.com/yaowen/p/3757571.html
 
-	public static String JSESSIONID = "JSESSIONID";
+    public static String JSESSIONID = "JSESSIONID";
 
-	public static BasicClientCookie getJSessionID(HttpResponseConfig httpResponseConfig) {
-		Map<String, String> responseHeaders = httpResponseConfig.getResponseHeaders();
-		String setCookie = responseHeaders.get("Set-Cookie");
-		String jSessionId = setCookie.substring((JSESSIONID + "=").length(), setCookie.indexOf(";"));
-		BasicClientCookie cookie = new BasicClientCookie(JSESSIONID, jSessionId);
-		// XXX set cookie properties
-		return cookie;
-	}
+    public static BasicClientCookie getJSessionID(HttpResponseConfig httpResponseConfig) {
+        Map<String, String> responseHeaders = httpResponseConfig.getResponseHeaders();
+        String setCookie = responseHeaders.get("Set-Cookie");
+        String jSessionId = setCookie.substring((JSESSIONID + "=").length(), setCookie.indexOf(";"));
+        BasicClientCookie cookie = new BasicClientCookie(JSESSIONID, jSessionId);
+        // XXX set cookie properties
+        return cookie;
+    }
 
-	public static BasicCookieStore buildCookieStore(BasicClientCookie cookie) {
-		BasicCookieStore cookieStore = new BasicCookieStore();
-		cookieStore.addCookie(cookie);
-		return cookieStore;
-	}
+    public static BasicCookieStore buildCookieStore(BasicClientCookie cookie) {
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        cookieStore.addCookie(cookie);
+        return cookieStore;
+    }
 
-	public static BasicCookieStore buildCookieStore(List<BasicClientCookie> cookies) {
-		BasicCookieStore cookieStore = new BasicCookieStore();
-		cookies.forEach(arg -> {
-			cookieStore.addCookie(arg);
-		});
-		return cookieStore;
-	}
+    public static BasicCookieStore buildCookieStore(List<BasicClientCookie> cookies) {
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        cookies.forEach(arg -> {
+            cookieStore.addCookie(arg);
+        });
+        return cookieStore;
+    }
 
-	private static URIBuilder setParam(Map<String, String> param, URIBuilder builder) {
-		param.forEach((k, v) -> {
-			builder.addParameter(k, v);
-		});
-		return builder;
-	}
+    private static URIBuilder setParam(Map<String, String> param, URIBuilder builder) {
+        param.forEach((k, v) -> {
+            builder.addParameter(k, v);
+        });
+        return builder;
+    }
 
-	private static Header[] setHeaders(Map<String, String> headersMap) {
-		List<BasicHeader> headerList = new ArrayList<>();
-		headersMap.forEach((k, v) -> {
-			headerList.add(new BasicHeader(k, v));
-		});
+    private static Header[] setHeaders(Map<String, String> headersMap) {
+        List<BasicHeader> headerList = new ArrayList<>();
+        headersMap.forEach((k, v) -> {
+            headerList.add(new BasicHeader(k, v));
+        });
 
-		Header[] headers = new Header[headerList.size()];
-		for (int i = 0; i < headerList.size(); i++) {
-			headers[i] = headerList.get(i);
-		}
-		return headers;
-	}
+        Header[] headers = new Header[headerList.size()];
+        for (int i = 0; i < headerList.size(); i++) {
+            headers[i] = headerList.get(i);
+        }
+        return headers;
+    }
 
-	private static Map<String, String> setHeadersMap(Header[] headersArray) {
-		Map<String, String> map = new HashMap<>();
-		for (Header header : headersArray) {
-			map.put(header.getName(), header.getValue());
-		}
-		return map;
-	}
+    private static Map<String, String> setHeadersMap(Header[] headersArray) {
+        Map<String, String> map = new HashMap<>();
+        for (Header header : headersArray) {
+            map.put(header.getName(), header.getValue());
+        }
+        return map;
+    }
 
-	private static String getContent(InputStream stream) throws IOException {
-		BufferedReader reader = null;
-		StringBuilder builder = new StringBuilder();
-		try {
-			reader = new BufferedReader(new InputStreamReader(stream));
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return builder.toString();
-	}
+    private static String getContent(InputStream stream) throws IOException {
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.toString();
+    }
 
 }
