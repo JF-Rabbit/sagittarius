e573cb5ba65dbbb9911a1bff609bcb821497eb82
JFRabbit <qwertjkl9090@163.com>
2017-11-09 13:53:48 +0800

    change getDriver method to (void) initDriver, add method getDriver, return private driver

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/driver/DriverManager.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/driver/DriverManager.java
@@ -24,157 +24,162 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
-@PropertySource({ DriverConstant.SELENIUM_CONFIG_PATH })
+@PropertySource({DriverConstant.SELENIUM_CONFIG_PATH})
 public class DriverManager implements DriverConstant {
 
-	@Value(DRIVER_TYPE)
-	private String driverType;
+    @Value(DRIVER_TYPE)
+    private String driverType;
 
-	@Value("${" + WEBDRIVER_CHROME_DRIVER + "}")
-	private String chromeDriverPath;
+    @Value("${" + WEBDRIVER_CHROME_DRIVER + "}")
+    private String chromeDriverPath;
 
-	@Value("${" + WEBDRIVER_GECKO_DRIVER + "}")
-	private String geckoDriverPath;
+    @Value("${" + WEBDRIVER_GECKO_DRIVER + "}")
+    private String geckoDriverPath;
 
-	@Value("${" + WEBDRIVER_FIREFOX_BIN + "}")
-	private String firefoxDriverPath;
+    @Value("${" + WEBDRIVER_FIREFOX_BIN + "}")
+    private String firefoxDriverPath;
 
-	@Value(REMOTE_HUB)
-	private String remoteHub;
+    @Value(REMOTE_HUB)
+    private String remoteHub;
 
-	@Value(WEBDRIVER_POINT_X)
-	private String browserX;
+    @Value(WEBDRIVER_POINT_X)
+    private String browserX;
 
-	@Value(WEBDRIVER_POINT_Y)
-	private String browserY;
+    @Value(WEBDRIVER_POINT_Y)
+    private String browserY;
 
-	@Value(WEBDRIVER_Dimension_WIDTH)
-	private String browserWidth;
+    @Value(WEBDRIVER_Dimension_WIDTH)
+    private String browserWidth;
 
-	@Value(WEBDRIVER_Dimension_HEIGHT)
-	private String browserHeigh;
+    @Value(WEBDRIVER_Dimension_HEIGHT)
+    private String browserHeigh;
 
-	@Value(SELENIUM_FIND_ELEMENT_TIMEOUT)
-	public Object findElementTimeout;
-	
-	@Value(SELENIUM_PAGE_LOAD_TIMEOUT)
-	public Object loadPageTimeout;
+    @Value(SELENIUM_FIND_ELEMENT_TIMEOUT)
+    public Object findElementTimeout;
 
-	private Browser browser;
+    @Value(SELENIUM_PAGE_LOAD_TIMEOUT)
+    public Object loadPageTimeout;
 
-	public Browser getBrowser() {
-		return browser;
-	}
+    private Browser browser;
 
-	public enum DriverType {
-		LOCAL_CHROME, REMOTE_CHROME, LOCAL_FIREFOX,
-	}
+    public Browser getBrowser() {
+        return browser;
+    }
 
-	private DriverType loadDriverTypeFromSource() throws DriverInitException {
+    public enum DriverType {
+        LOCAL_CHROME, REMOTE_CHROME, LOCAL_FIREFOX,
+    }
 
-		if (StringUtils.isEmpty(driverType)) {
-			throw new DriverInitException(DRIVER_TYPE_NOT_FOUND_IN_PROPERTIES);
-		}
+    private DriverType loadDriverTypeFromSource() throws DriverInitException {
 
-		DriverType type = null;
-		try {
-			type = DriverType.valueOf(driverType);
-		} catch (IllegalArgumentException e) {
-			throw new DriverInitException(DRIVER_TYPE_WRONG_ENUM, e);
-		}
+        if (StringUtils.isEmpty(driverType)) {
+            throw new DriverInitException(DRIVER_TYPE_NOT_FOUND_IN_PROPERTIES);
+        }
 
-		return type;
-	}
+        DriverType type = null;
+        try {
+            type = DriverType.valueOf(driverType);
+        } catch (IllegalArgumentException e) {
+            throw new DriverInitException(DRIVER_TYPE_WRONG_ENUM, e);
+        }
 
-	private void setChromeConfig() throws DriverInitException {
-		if (StringUtils.isEmpty(chromeDriverPath)) {
-			throw new DriverInitException(WEBDRIVER_CHROME_DRIVER_NOT_FOUND);
-		}
-		System.setProperty(WEBDRIVER_CHROME_DRIVER, chromeDriverPath);
-	}
+        return type;
+    }
 
-	private void setFirefoxConfig() throws DriverInitException {
+    private void setChromeConfig() throws DriverInitException {
+        if (StringUtils.isEmpty(chromeDriverPath)) {
+            throw new DriverInitException(WEBDRIVER_CHROME_DRIVER_NOT_FOUND);
+        }
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, chromeDriverPath);
+    }
 
-		if (StringUtils.isEmpty(geckoDriverPath)) {
-			throw new DriverInitException(WEBDRIVER_GECKO_DRIVER_NOT_FOUND);
-		}
-		if (StringUtils.isEmpty(firefoxDriverPath)) {
-			throw new DriverInitException(WEBDRIVER_FIREFOX_BIN_NOT_FOUND);
-		}
-		System.setProperty(WEBDRIVER_GECKO_DRIVER, geckoDriverPath);
-		System.setProperty(WEBDRIVER_FIREFOX_BIN, firefoxDriverPath);
-	}
+    private void setFirefoxConfig() throws DriverInitException {
 
-	private String getRemoteHubAddressFromSource() throws DriverInitException {
-		if (StringUtils.isEmpty(remoteHub)) {
-			throw new DriverInitException(REMOVTE_HUB_NOT_FOUND_IN_PROPERTIES);
-		}
-		return remoteHub;
-	}
+        if (StringUtils.isEmpty(geckoDriverPath)) {
+            throw new DriverInitException(WEBDRIVER_GECKO_DRIVER_NOT_FOUND);
+        }
+        if (StringUtils.isEmpty(firefoxDriverPath)) {
+            throw new DriverInitException(WEBDRIVER_FIREFOX_BIN_NOT_FOUND);
+        }
+        System.setProperty(WEBDRIVER_GECKO_DRIVER, geckoDriverPath);
+        System.setProperty(WEBDRIVER_FIREFOX_BIN, firefoxDriverPath);
+    }
 
-	private RemoteWebDriver setRemoteWebDriver(DesiredCapabilities capabilities) throws DriverInitException {
-		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-		capabilities.setCapability("name", "Remote File Upload using Selenium 2's FileDetectors");
-		RemoteWebDriver remoteWebDriver = null;
-		try {
-			remoteWebDriver = new RemoteWebDriver(new URL(getRemoteHubAddressFromSource()), capabilities);
-		} catch (MalformedURLException e) {
-			throw new DriverInitException(REMOVTE_HUB_CONNECT_ERROR, e);
-		}
-		remoteWebDriver.setFileDetector(new LocalFileDetector());
-		return remoteWebDriver;
-	}
-	
-	public static int find_element_timeout = DEFAULT_SELENIUM_FIND_ELEMENT_TIMEOUT;
-	
-	private int initFindElementTimeout() throws DriverInitException {
-		if(findElementTimeout != null) {
-			find_element_timeout = Integer.valueOf(String.valueOf(findElementTimeout));
-			return find_element_timeout;
-		}
-		return DEFAULT_SELENIUM_FIND_ELEMENT_TIMEOUT;
-	}
-	
-	private int initSeleniumPageLoadTimeout() throws DriverInitException {
-		if(loadPageTimeout != null) {
-			return Integer.valueOf(String.valueOf(loadPageTimeout));
-		} 
-		return DEFAULT_SELENIUM_PAGE_LOAD_TIMEOUT;
-	}
+    private String getRemoteHubAddressFromSource() throws DriverInitException {
+        if (StringUtils.isEmpty(remoteHub)) {
+            throw new DriverInitException(REMOVTE_HUB_NOT_FOUND_IN_PROPERTIES);
+        }
+        return remoteHub;
+    }
 
-	public WebDriver getDriver() throws DriverInitException {
-		DriverType driverType = this.loadDriverTypeFromSource();
+    private RemoteWebDriver setRemoteWebDriver(DesiredCapabilities capabilities) throws DriverInitException {
+        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        capabilities.setCapability("name", "Remote File Upload using Selenium 2's FileDetectors");
+        RemoteWebDriver remoteWebDriver = null;
+        try {
+            remoteWebDriver = new RemoteWebDriver(new URL(getRemoteHubAddressFromSource()), capabilities);
+        } catch (MalformedURLException e) {
+            throw new DriverInitException(REMOVTE_HUB_CONNECT_ERROR, e);
+        }
+        remoteWebDriver.setFileDetector(new LocalFileDetector());
+        return remoteWebDriver;
+    }
 
-		WebDriver driver = null;
-		DesiredCapabilities capabilities = null;
-		switch (driverType) {
-		case LOCAL_CHROME:
-			this.setChromeConfig();
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("disable-infobars");
-			driver = new ChromeDriver(options);
-			this.browser = Browser.CHROME;
-			driver = setBrowserLayout(driver);
-			break;
-		case REMOTE_CHROME:
-			capabilities = DesiredCapabilities.chrome();
-			driver = setRemoteWebDriver(capabilities);
-			break;
-		case LOCAL_FIREFOX:
-			this.setFirefoxConfig();
-			// skip SSL
-			// ProfilesIni profilesIni = new ProfilesIni();
-			// FirefoxProfile profile = profilesIni.getProfile("default");
-			// profile.setAcceptUntrustedCertificates(true);
-			// profile.setAssumeUntrustedCertificateIssuer(true);
+    public static int find_element_timeout = DEFAULT_SELENIUM_FIND_ELEMENT_TIMEOUT;
 
-			// FirefoxOptions options = new FirefoxOptions();
-			// options.setProfile(profile);
+    private int initFindElementTimeout() throws DriverInitException {
+        if (findElementTimeout != null) {
+            find_element_timeout = Integer.valueOf(String.valueOf(findElementTimeout));
+            return find_element_timeout;
+        }
+        return DEFAULT_SELENIUM_FIND_ELEMENT_TIMEOUT;
+    }
 
-			capabilities = DesiredCapabilities.firefox();
+    private int initSeleniumPageLoadTimeout() throws DriverInitException {
+        if (loadPageTimeout != null) {
+            return Integer.valueOf(String.valueOf(loadPageTimeout));
+        }
+        return DEFAULT_SELENIUM_PAGE_LOAD_TIMEOUT;
+    }
 
-			driver = new FirefoxDriver(capabilities);
-			/*
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+
+    public void initDriver() throws DriverInitException {
+        DriverType driverType = this.loadDriverTypeFromSource();
+
+        DesiredCapabilities capabilities = null;
+        switch (driverType) {
+            case LOCAL_CHROME:
+                this.setChromeConfig();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("disable-infobars");
+                driver = new ChromeDriver(options);
+                this.browser = Browser.CHROME;
+                driver = setBrowserLayout(driver);
+                break;
+            case REMOTE_CHROME:
+                capabilities = DesiredCapabilities.chrome();
+                driver = setRemoteWebDriver(capabilities);
+                break;
+            case LOCAL_FIREFOX:
+                this.setFirefoxConfig();
+                // skip SSL
+                // ProfilesIni profilesIni = new ProfilesIni();
+                // FirefoxProfile profile = profilesIni.getProfile("default");
+                // profile.setAcceptUntrustedCertificates(true);
+                // profile.setAssumeUntrustedCertificateIssuer(true);
+
+                // FirefoxOptions options = new FirefoxOptions();
+                // options.setProfile(profile);
+
+                capabilities = DesiredCapabilities.firefox();
+
+                driver = new FirefoxDriver(capabilities);
+            /*
 			 * XXX can't set maximize for firefox
 			 * 
 			 * use maximize history manually to skip
@@ -182,44 +187,43 @@ public class DriverManager implements DriverConstant {
 			 * github bug: https://github.com/mozilla/geckodriver/issues/820
 			 * 
 			 */
-			driver = setBrowserLayout(driver);
-			this.browser = Browser.FIREFOX;
-			break;
-		}
+                driver = setBrowserLayout(driver);
+                this.browser = Browser.FIREFOX;
+                break;
+        }
 
-		if (driver == null) {
-			throw new DriverInitException(DRIVER_IS_NULL);
-		}
+        if (driver == null) {
+            throw new DriverInitException(DRIVER_IS_NULL);
+        }
 
-		driver.manage().timeouts().implicitlyWait(initFindElementTimeout(), TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(initSeleniumPageLoadTimeout(), TimeUnit.SECONDS);
-		
-		return driver;
-	}
+        driver.manage().timeouts().implicitlyWait(initFindElementTimeout(), TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(initSeleniumPageLoadTimeout(), TimeUnit.SECONDS);
 
-	private WebDriver setBrowserLayout(WebDriver driver) throws DriverInitException {
+    }
 
-		WebDriver.Window window = driver.manage().window();
-		if (StringUtils.isNotEmpty(browserX) && StringUtils.isNotEmpty(browserY)) {
-			window.setPosition(new Point(Integer.valueOf(browserX), Integer.valueOf(browserY)));
-		}
+    private WebDriver setBrowserLayout(WebDriver driver) throws DriverInitException {
 
-		if (StringUtils.isNotEmpty(browserWidth) && StringUtils.isNotEmpty(browserHeigh)) {
-			window.setSize(new Dimension(Integer.valueOf(browserWidth), Integer.valueOf(browserHeigh)));
-		} else {
-			if (browser != Browser.CHROME) {
-				throw new DriverInitException(
-						"can't set maximize for firefox, github bug: https://github.com/mozilla/geckodriver/issues/820");
-			}
-			window.maximize();
-		}
+        WebDriver.Window window = driver.manage().window();
+        if (StringUtils.isNotEmpty(browserX) && StringUtils.isNotEmpty(browserY)) {
+            window.setPosition(new Point(Integer.valueOf(browserX), Integer.valueOf(browserY)));
+        }
 
-		return driver;
-	}
+        if (StringUtils.isNotEmpty(browserWidth) && StringUtils.isNotEmpty(browserHeigh)) {
+            window.setSize(new Dimension(Integer.valueOf(browserWidth), Integer.valueOf(browserHeigh)));
+        } else {
+            if (browser != Browser.CHROME) {
+                throw new DriverInitException(
+                        "can't set maximize for firefox, github bug: https://github.com/mozilla/geckodriver/issues/820");
+            }
+            window.maximize();
+        }
 
-	public void quitDriver(WebDriver driver) {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+        return driver;
+    }
+
+    public void quitDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
