a5dd361d002219025c34fe060a9af82b6a73486f
JFRabbit <qwertjkl9090@163.com>
2017-12-05 14:07:34 +0800

    update findElementByWait Function

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/web/WebElementUtil.java
@@ -16,187 +16,140 @@ import org.slf4j.LoggerFactory;
 
 public class WebElementUtil {
 
-	private static final Logger logger = LoggerFactory.getLogger(WebElementUtil.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebElementUtil.class);
 
-	public static int[] getElementCenter(WebElement element) {
-		int x = element.getLocation().x;
-		int y = element.getLocation().y;
-		int height = element.getSize().height;
-		int width = element.getSize().width;
-		logger.info("x:{}, y:{}, height:{}, width:{}", x, y, height, width);
-		return new int[] { x + width / 2, y + height / 2 };
-	}
+    public static int[] getElementCenter(WebElement element) {
+        int x = element.getLocation().x;
+        int y = element.getLocation().y;
+        int height = element.getSize().height;
+        int width = element.getSize().width;
+        logger.info("x:{}, y:{}, height:{}, width:{}", x, y, height, width);
+        return new int[]{x + width / 2, y + height / 2};
+    }
 
-	private static final String ELEMENT_NOT_VISIBLE_MSG = "element is not visible:";
-	private static final String ELEMENT_IS_VISIBLE_MSG = "element is visible, please inspect:";
+    private static final String ELEMENT_NOT_VISIBLE_MSG = "element is not visible:";
+    private static final String ELEMENT_IS_VISIBLE_MSG = "element is visible, please inspect:";
 
-	@Deprecated
-	public static void clickDisableElement(WebDriver driver, WebElement element) {
-		if (element.isDisplayed() == false) {
-			logger.warn(ELEMENT_NOT_VISIBLE_MSG + "{}", element);
-			// PageElementUtil.showPageSource(driver);
-		} else {
-			logger.warn(ELEMENT_IS_VISIBLE_MSG + "{}", element);
-		}
-		int[] coordinate = getElementCenter(element);
-		logger.info("screenX:{}, screenY:{}", coordinate[0], coordinate[1] + JsUtil.getActualY(driver));
-		RobotUtil.moveToClick(coordinate[0], coordinate[1] + JsUtil.getActualY(driver));
-	}
+    @Deprecated
+    public static void clickDisableElement(WebDriver driver, WebElement element) {
+        if (!element.isDisplayed()) {
+            logger.warn(ELEMENT_NOT_VISIBLE_MSG + "{}", element);
+            // PageElementUtil.showPageSource(driver);
+        } else {
+            logger.warn(ELEMENT_IS_VISIBLE_MSG + "{}", element);
+        }
+        int[] coordinate = getElementCenter(element);
+        logger.info("screenX:{}, screenY:{}", coordinate[0], coordinate[1] + JsUtil.getActualY(driver));
+        RobotUtil.moveToClick(coordinate[0], coordinate[1] + JsUtil.getActualY(driver));
+    }
 
-	private static final String MULTIPLE_CLICKABLE_ELEMENT = "MULTIPLE_CLICKABLE_ELEMENT";
-	private static final String NO_DISPLAYED_ELEMENT = "NO_DISPLAYED_ELEMENT";
+    private static final String MULTIPLE_CLICKABLE_ELEMENT = "MULTIPLE_CLICKABLE_ELEMENT";
+    private static final String NO_DISPLAYED_ELEMENT = "NO_DISPLAYED_ELEMENT";
+    private static final String FIND_BY_WAIT_WARN = "findByWait timeout should > DriverManager.find_element_timeout";
 
-	public static void clickOneDisplayedElementofList(List<WebElement> elementList) {
-		List<WebElement> list = new ArrayList<WebElement>();
-		for (int i = 0; i < elementList.size(); i++) {
-			logger.info("index:{}, isDisplayed:{}, element:{}", i, elementList.get(i).isDisplayed(), elementList.get(i));
-			if (elementList.get(i).isDisplayed()) {
-				list.add(elementList.get(i));
-			}
-		}
+    public static void clickOneDisplayedElementofList(List<WebElement> elementList) {
+        List<WebElement> list = new ArrayList<>();
+        for (int i = 0; i < elementList.size(); i++) {
+            logger.info("index:{}, isDisplayed:{}, element:{}", i, elementList.get(i).isDisplayed(), elementList.get(i));
+            if (elementList.get(i).isDisplayed()) {
+                list.add(elementList.get(i));
+            }
+        }
 
-		if (list.size() == 0) {
-			throw new IllegalArgumentException(NO_DISPLAYED_ELEMENT);
-		} else if (list.size() == 1) {
-			list.get(0).click();
-		} else {
-			throw new IllegalArgumentException(MULTIPLE_CLICKABLE_ELEMENT);
-		}
+        if (list.size() == 0) {
+            throw new IllegalArgumentException(NO_DISPLAYED_ELEMENT);
+        } else if (list.size() == 1) {
+            list.get(0).click();
+        } else {
+            throw new IllegalArgumentException(MULTIPLE_CLICKABLE_ELEMENT);
+        }
 
-	}
+    }
 
-	private static void checkTimeout(int secound) {
-		if (secound <= Integer.valueOf(String.valueOf(DriverManager.find_element_timeout))) {
-			throw new IllegalArgumentException(MULTIPLE_CLICKABLE_ELEMENT);
-		}
-	}
+    private static void checkTimeout(int secound) {
+        if (secound <= Integer.valueOf(String.valueOf(DriverManager.find_element_timeout))) {
+            throw new IllegalArgumentException(FIND_BY_WAIT_WARN);
+        }
+    }
 
-	public static WebElement findElement(WebDriver driver, WebByEnum by, String msg) {
+    private static final String NOT_IMPL = "key is not implement!";
 
-		switch (by) {
-		case XPATH:
-			return driver.findElement(By.xpath(msg));
-		case ID:
-			return driver.findElement(By.id(msg));
-		case CLASS_NAME:
-			return driver.findElement(By.className(msg));
-		case NAME:
-			return driver.findElement(By.name(msg));
-		case TAG_NAME:
-			return driver.findElement(By.tagName(msg));
-		case LINK_TEXT:
-			return driver.findElement(By.linkText(msg));
-		case CSS:
-			return driver.findElement(By.cssSelector(msg));
-		case PARTIA_LINK_TEXT:
-			return driver.findElement(By.partialLinkText(msg));
-		default:
-			return null;
-		}
-	}
+    public static WebElement findElement(WebDriver driver, WebByEnum by, String msg) {
 
-	public static List<WebElement> findElements(WebDriver driver, WebByEnum by, String msg) {
+        switch (by) {
+            case XPATH:
+                return driver.findElement(By.xpath(msg));
+            case ID:
+                return driver.findElement(By.id(msg));
+            case CLASS_NAME:
+                return driver.findElement(By.className(msg));
+            case NAME:
+                return driver.findElement(By.name(msg));
+            case TAG_NAME:
+                return driver.findElement(By.tagName(msg));
+            case LINK_TEXT:
+                return driver.findElement(By.linkText(msg));
+            case CSS:
+                return driver.findElement(By.cssSelector(msg));
+            case PARTIA_LINK_TEXT:
+                return driver.findElement(By.partialLinkText(msg));
+            default:
+                throw new IllegalArgumentException(NOT_IMPL);
+        }
+    }
 
-		switch (by) {
-		case XPATH:
-			return driver.findElements(By.xpath(msg));
-		case ID:
-			return driver.findElements(By.id(msg));
-		case CLASS_NAME:
-			return driver.findElements(By.className(msg));
-		case NAME:
-			return driver.findElements(By.name(msg));
-		case TAG_NAME:
-			return driver.findElements(By.tagName(msg));
-		case LINK_TEXT:
-			return driver.findElements(By.linkText(msg));
-		case CSS:
-			return driver.findElements(By.cssSelector(msg));
-		case PARTIA_LINK_TEXT:
-			return driver.findElements(By.partialLinkText(msg));
-		default:
-			return null;
-		}
-	}
+    public static List<WebElement> findElements(WebDriver driver, WebByEnum by, String msg) {
 
-	public static WebElement findElementByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
+        switch (by) {
+            case XPATH:
+                return driver.findElements(By.xpath(msg));
+            case ID:
+                return driver.findElements(By.id(msg));
+            case CLASS_NAME:
+                return driver.findElements(By.className(msg));
+            case NAME:
+                return driver.findElements(By.name(msg));
+            case TAG_NAME:
+                return driver.findElements(By.tagName(msg));
+            case LINK_TEXT:
+                return driver.findElements(By.linkText(msg));
+            case CSS:
+                return driver.findElements(By.cssSelector(msg));
+            case PARTIA_LINK_TEXT:
+                return driver.findElements(By.partialLinkText(msg));
+            default:
+                throw new IllegalArgumentException(NOT_IMPL);
+        }
+    }
 
-		checkTimeout(secound);
+    public static WebElement findElementByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
+        checkTimeout(secound);
+        WebDriverWait wait = new WebDriverWait(driver, secound);
+        return wait.until(d -> findElement(d, by, msg));
+    }
 
-		WebDriverWait wait = new WebDriverWait(driver, secound);
-		return wait.until(new ExpectedCondition<WebElement>() {
-			public WebElement apply(WebDriver driver) {
-				switch (by) {
-				case XPATH:
-					return driver.findElement(By.xpath(msg));
-				case ID:
-					return driver.findElement(By.id(msg));
-				case CLASS_NAME:
-					return driver.findElement(By.className(msg));
-				case NAME:
-					return driver.findElement(By.name(msg));
-				case TAG_NAME:
-					return driver.findElement(By.tagName(msg));
-				case LINK_TEXT:
-					return driver.findElement(By.linkText(msg));
-				case CSS:
-					return driver.findElement(By.cssSelector(msg));
-				case PARTIA_LINK_TEXT:
-					return driver.findElement(By.partialLinkText(msg));
-				default:
-					return null;
-				}
-			}
-		});
-	}
+    public static List<WebElement> findElementsByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
+        checkTimeout(secound);
+        WebDriverWait wait = new WebDriverWait(driver, secound);
+        return wait.until(d -> findElements(d, by, msg));
+    }
 
-	public static List<WebElement> findElementsByWait(WebDriver driver, int secound, WebByEnum by, String msg) {
+    public static void saveCurrentWebPage(String path) {
+        RobotUtil.setClipboardData(path);
+        RobotUtil.action_CONTROL_S_CONTROL_V_ENTER();
+    }
 
-		checkTimeout(secound);
+    public static boolean isExsit(WebDriver driver, WebByEnum by, String msg) {
+        try {
+            WebElement element = findElement(driver, by, msg);
+            if (element.isDisplayed()) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
 
-		WebDriverWait wait = new WebDriverWait(driver, secound);
-		return wait.until(new ExpectedCondition<List<WebElement>>() {
-			public List<WebElement> apply(WebDriver driver) {
-				switch (by) {
-				case XPATH:
-					return driver.findElements(By.xpath(msg));
-				case ID:
-					return driver.findElements(By.id(msg));
-				case CLASS_NAME:
-					return driver.findElements(By.className(msg));
-				case NAME:
-					return driver.findElements(By.name(msg));
-				case TAG_NAME:
-					return driver.findElements(By.tagName(msg));
-				case LINK_TEXT:
-					return driver.findElements(By.linkText(msg));
-				case CSS:
-					return driver.findElements(By.cssSelector(msg));
-				case PARTIA_LINK_TEXT:
-					return driver.findElements(By.partialLinkText(msg));
-				default:
-					return null;
-				}
-			}
-		});
-	}
-
-	public static void saveCurrentWebPage(String path) {
-		RobotUtil.setClipboardData(path);
-		RobotUtil.action_CONTROL_S_CONTROL_V_ENTER();
-	}
-
-	public static boolean isExsit(WebDriver driver, WebByEnum by, String msg) {
-		try {
-			WebElement element = findElement(driver, by, msg);
-			if (element.isDisplayed()) {
-				return false;
-			}
-		} catch (Exception e) {
-			return false;
-		}
-
-		return true;
-	}
+        return true;
+    }
 
 }
--- sagittarius-uitest/src/test/java/org/sagittarius/test/TestDrivarManager.java
+++ sagittarius-uitest/src/test/java/org/sagittarius/test/TestDrivarManager.java
@@ -2,6 +2,8 @@ package org.sagittarius.test;
 
 import org.sagittarius.common.Delay;
 import org.sagittarius.uitest.WebTest;
+import org.sagittarius.uitest.util.web.WebByEnum;
+import org.sagittarius.uitest.util.web.WebElementUtil;
 import org.testng.annotations.Test;
 
 public class TestDrivarManager extends WebTest{
@@ -10,6 +12,12 @@ public class TestDrivarManager extends WebTest{
 	public void doTest1() {
 		driver.get("http://www.baidu.com");
 		Delay.sleep(3000);
+	}
+
+	@Test
+	public void testWebDrivrWait() {
+		driver.get("http://www.baidu.com");
+		WebElementUtil.findElementByWait(driver, 5, WebByEnum.ID, "su");
 	}
 
 }
