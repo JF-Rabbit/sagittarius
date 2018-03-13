8e93eeb203c8620e8ac88806002986b4baf41841
JFRabbit <qwertjkl9090@163.com>
2018-03-13 10:44:49 +0800

    update FunctionUtil

--- sagittarius-common/src/main/java/org/sagittarius/common/FunctionUtil.java
+++ sagittarius-common/src/main/java/org/sagittarius/common/FunctionUtil.java
@@ -2,29 +2,39 @@ package org.sagittarius.common;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 public interface FunctionUtil {
 
-    Predicate<Object> isNull = n -> n == null;
+    Predicate<Object> IS_NULL = n -> n == null;
+    Predicate<Object> NOT_NULL = n -> IS_NULL.negate().test(n);
 
-    Predicate<String> strNull = n -> n == null;
-    Predicate<String> strEmpty = n -> n.trim().length() == 0;
+    Predicate<String> STR_NULL = n -> n == null;
+    Predicate<String> STR_EMPTY = n -> n.trim().length() == 0;
 
-    Predicate<List<?>> listNull = n -> n == null;
-    Predicate<List<?>> listEmpty = n -> n.size() == 0;
+    Predicate<List<?>> LIST_NULL = n -> n == null;
+    Predicate<List<?>> LIST_EMPTY = n -> n.size() == 0;
 
-    Predicate<Map<?, ?>> mapNull = n -> n == null;
-    Predicate<Map<?, ?>> mapEmpty = n -> n.size() == 0;
+    Predicate<Map<?, ?>> MAP_NULL = n -> n == null;
+    Predicate<Map<?, ?>> MAP_EMPTY = n -> n.size() == 0;
 
-    Function<String, Boolean> EMPTY_STR = n -> strNull.or(strEmpty).test(n);
-    Function<String, Boolean> NOT_EMPTY_STR = n -> strNull.negate().and(strEmpty.negate()).test(n);
+    Predicate<String> EMPTY_STR = n -> STR_NULL.or(STR_EMPTY).test(n);
+    Predicate<String> NOT_EMPTY_STR = n -> EMPTY_STR.negate().test(n);
 
-    Function<List<?>, Boolean> EMPTY_LIST = n -> listNull.or(listEmpty).test(n);
-    Function<List<?>, Boolean> NOT_EMPTY_LIST = n -> listNull.negate().and(listEmpty.negate()).test(n);
+    Predicate<List<?>> EMPTY_LIST = n -> LIST_NULL.or(LIST_EMPTY).test(n);
+    Predicate<List<?>> NOT_EMPTY_LIST = n -> EMPTY_LIST.negate().test(n);
 
-    Function<Map<?, ?>, Boolean> EMPTY_MAP = n -> mapNull.or(mapEmpty).test(n);
-    Function<Map<?, ?>, Boolean> NOT_EMPTY_MAP = n -> mapNull.negate().and(mapEmpty.negate()).test(n);
+    Predicate<Map<?, ?>> EMPTY_MAP = n -> MAP_NULL.or(MAP_EMPTY).test(n);
+    Predicate<Map<?, ?>> NOT_EMPTY_MAP = n -> EMPTY_MAP.negate().test(n);
+
+    static void NULL_THROW(Object obj, Exception e) throws Exception {
+        if (IS_NULL.test(obj))
+            throw e;
+    }
+
+    static void NULL_THROW(Object obj, RuntimeException e) {
+        if (IS_NULL.test(obj))
+            throw e;
+    }
 
 }
