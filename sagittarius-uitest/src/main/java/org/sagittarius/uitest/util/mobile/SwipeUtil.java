a524e79a8a123efc6ca102e3b71202f2af86ff77
JFRabbit <qwertjkl9090@163.com>
2018-08-24 20:19:25 +0800

    feat: 兼容selenium3.12.0

--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/mobile/SwipeUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/mobile/SwipeUtil.java
@@ -7,44 +7,36 @@ import io.appium.java_client.android.AndroidDriver;
 
 public interface SwipeUtil {
 
-	/**
-	 * 由下滑到上
-	 * 
-	 * @param andriodDriver
-	 * @param element
-	 * @param duration
-	 */
-	default void swipeBottomToTop(AndroidDriver<?> andriodDriver, MobileElement element, int duration) {
-		int startx = (element.getLocation().x + element.getSize().width) / 2;
-		int starty = element.getLocation().y + element.getSize().height;
-		int endx = startx;
-		int endy = element.getLocation().y;
-		andriodDriver.swipe(startx, starty, endx, endy, duration);
-	}
+    /**
+     * 由下滑到上
+     */
+    default void swipeBottomToTop(AndroidDriver<?> androidDriver, MobileElement element, int duration) {
+        int startx = (element.getLocation().x + element.getSize().width) / 2;
+        int starty = element.getLocation().y + element.getSize().height;
+        int endx = startx;
+        int endy = element.getLocation().y;
+        TouchActionUtil.swipe(androidDriver, startx, starty, endx, endy, duration);
+    }
 
-	/**
-	 * 由右滑到左
-	 * 
-	 * @param andriodDriver
-	 * @param element
-	 * @param duration
-	 */
-	default void swipeRightToLeft(AndroidDriver<?> andriodDriver, MobileElement element, int duration) {
-		int startx = element.getLocation().x + element.getSize().width;
-		int starty = (element.getLocation().y + element.getSize().height) / 2;
-		int endx = element.getLocation().x;
-		int endy = starty;
-		andriodDriver.swipe(startx, starty, endx, endy, duration);
-	}
+    /**
+     * 由右滑到左
+     */
+    default void swipeRightToLeft(AndroidDriver<?> androidDriver, MobileElement element, int duration) {
+        int startx = element.getLocation().x + element.getSize().width;
+        int starty = (element.getLocation().y + element.getSize().height) / 2;
+        int endx = element.getLocation().x;
+        int endy = starty;
+        TouchActionUtil.swipe(androidDriver, startx, starty, endx, endy, duration);
+    }
 
-	default void swipeOneElementToAnother(AndroidDriver<?> andriodDriver, MobileElement one, MobileElement another,
-			int duration) throws SwipeLocationException {
-		if ((one.getLocation().x == 0 && one.getLocation().y == 0)
-				|| (another.getLocation().x == 0 && another.getLocation().y == 0)) {
-			throw new SwipeLocationException("x, y不能都为0");
-		}
-		andriodDriver.swipe(one.getLocation().x + one.getSize().width / 2,
-				one.getLocation().y + one.getSize().height / 2, another.getLocation().x + another.getSize().width / 2,
-				another.getLocation().y + another.getSize().height / 2, duration);
-	}
+    default void swipeOneElementToAnother(AndroidDriver<?> androidDriver, MobileElement one, MobileElement another,
+                                          int duration) throws SwipeLocationException {
+        if ((one.getLocation().x == 0 && one.getLocation().y == 0)
+                || (another.getLocation().x == 0 && another.getLocation().y == 0)) {
+            throw new SwipeLocationException("x, y不能都为0");
+        }
+        TouchActionUtil.swipe(androidDriver, one.getLocation().x + one.getSize().width / 2,
+                one.getLocation().y + one.getSize().height / 2, another.getLocation().x + another.getSize().width / 2,
+                another.getLocation().y + another.getSize().height / 2, duration);
+    }
 }
--- sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/mobile/TouchActionUtil.java
+++ sagittarius-uitest/src/main/java/org/sagittarius/uitest/util/mobile/TouchActionUtil.java
@@ -6,33 +6,43 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class TouchActionUtil {
-	public static void swipe(AppiumDriver<?> driver, int startx, int starty, int endx, int endy, int duration) {
-		if (driver instanceof AndroidDriver) {
-			new TouchAction(driver).press(startx, starty).waitAction(changeMillisToDuration(duration)).moveTo(endx, endy).release()
-					.perform();
-		}
-		if (driver instanceof IOSDriver) {
-			int xOffset = endx - startx;
-			int yOffset = endy - starty;
-			new TouchAction(driver).press(startx, starty).waitAction(changeMillisToDuration(duration)).moveTo(xOffset, yOffset).release()
-					.perform();
-		}
-	}
+    public static void swipe(AppiumDriver<?> driver, int startx, int starty, int endx, int endy, int duration) {
+        if (driver instanceof AndroidDriver) {
+            new TouchAction(driver)
+                    .press(PointOption.point(startx, starty))
+                    .waitAction(WaitOptions.waitOptions(changeMillisToDuration(duration)))
+                    .moveTo(PointOption.point(endx, endy))
+                    .release()
+                    .perform();
+        }
+        if (driver instanceof IOSDriver) {
+            int xOffset = endx - startx;
+            int yOffset = endy - starty;
+            new TouchAction(driver)
+                    .press(PointOption.point(startx, starty))
+                    .waitAction(WaitOptions.waitOptions(changeMillisToDuration(duration)))
+                    .moveTo(PointOption.point(xOffset, yOffset))
+                    .release()
+                    .perform();
+        }
+    }
 
-	public static void tap(AppiumDriver<?> driver, int x, int y) {
-		TouchAction tap = new TouchAction(driver);
-		tap.press(x, y).release().perform();
-	}
+    public static void tap(AppiumDriver<?> driver, int x, int y) {
+        TouchAction tap = new TouchAction(driver);
+        tap.press(PointOption.point(x, y)).release().perform();
+    }
 
-	/**
-	 * will support on java client BETA-7 or later
-	 * 
-	 * @param duration
-	 * @return
-	 */
-	private static Duration changeMillisToDuration(int duration) {
-		return Duration.ofMillis(duration);
-	}
+    /**
+     * will support on java client BETA-7 or later
+     *
+     * @param duration
+     * @return
+     */
+    private static Duration changeMillisToDuration(int duration) {
+        return Duration.ofMillis(duration);
+    }
 }
