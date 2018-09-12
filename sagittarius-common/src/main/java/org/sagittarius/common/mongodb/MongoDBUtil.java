5097cae7b9e6a05fe044ca0380864d9e230a5624
JFRabbit <qwertjkl9090@163.com>
2018-09-12 10:51:04 +0800

    fix: add no auth connection

--- sagittarius-common/src/main/java/org/sagittarius/common/mongodb/MongoDBUtil.java
+++ sagittarius-common/src/main/java/org/sagittarius/common/mongodb/MongoDBUtil.java
@@ -1,59 +1,66 @@
 package org.sagittarius.common.mongodb;
 
-import java.util.Arrays;
-
-import org.bson.Document;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.sagittarius.common.FunctionUtil;
+
+import java.util.Arrays;
 
 public class MongoDBUtil {
 
-	private MongoDBConfig config;
-	private MongoDBCollection mongoDBCollection;
-	private MongoClient mongoClient;
+    private MongoDBConfig config;
+    private MongoDBCollection mongoDBCollection;
+    private MongoClient mongoClient;
 
-	public MongoDBUtil() {
-		super();
-	}
+    public MongoDBUtil() {
+        super();
+    }
 
-	public MongoDBUtil(MongoDBConfig config, MongoDBCollection mongoDBCollection) {
-		super();
-		this.config = config;
-		this.mongoDBCollection = mongoDBCollection;
-	}
+    public MongoDBUtil(MongoDBConfig config, MongoDBCollection mongoDBCollection) {
+        super();
+        this.config = config;
+        this.mongoDBCollection = mongoDBCollection;
+    }
 
-	public MongoDBConfig getConfig() {
-		return config;
-	}
+    public MongoDBConfig getConfig() {
+        return config;
+    }
 
-	public void setConfig(MongoDBConfig config) {
-		this.config = config;
-	}
+    public void setConfig(MongoDBConfig config) {
+        this.config = config;
+    }
 
-	public MongoDBCollection getMongoDBCollection() {
-		return mongoDBCollection;
-	}
+    public MongoDBCollection getMongoDBCollection() {
+        return mongoDBCollection;
+    }
 
-	public void setMongoDBCollection(MongoDBCollection mongoDBCollection) {
-		this.mongoDBCollection = mongoDBCollection;
-	}
+    public void setMongoDBCollection(MongoDBCollection mongoDBCollection) {
+        this.mongoDBCollection = mongoDBCollection;
+    }
 
-	public MongoCollection<Document> connect() {
-		MongoCredential credential = MongoCredential.createScramSha1Credential(config.getUserName(), config.getDatabaseName(),
-				config.getPassword().toCharArray());
-		mongoClient = new MongoClient(new ServerAddress(config.getUrl(), config.getPort()), Arrays.asList(credential));
-		MongoDatabase database = mongoClient.getDatabase(config.getDatabaseName());
-		MongoCollection<Document> collection = database.getCollection(mongoDBCollection.getCollectionName());
-		return collection;
-	}
+    public MongoCollection<Document> connect() {
+        MongoCredential credential = null;
+        if (FunctionUtil.NOT_EMPTY_STR.test(config.getUserName()) && FunctionUtil.NOT_EMPTY_STR.test(config.getPassword())) {
+            credential = MongoCredential.createScramSha1Credential(config.getUserName(), config.getDatabaseName(),
+                    config.getPassword().toCharArray());
+        }
+        if (credential != null) {
+            mongoClient = new MongoClient(new ServerAddress(config.getUrl(), config.getPort()), Arrays.asList(credential));
+        } else {
+            mongoClient = new MongoClient(new ServerAddress(config.getUrl(), config.getPort()));
+        }
 
-	public void close() {
-		if (mongoClient != null) {
-			mongoClient.close();
-		}
-	}
+        MongoDatabase database = mongoClient.getDatabase(config.getDatabaseName());
+        return database.getCollection(mongoDBCollection.getCollectionName());
+    }
+
+    public void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
 }
